b09bb6193c9a82d61d3e0f283bda46955bac8a8d
unknown <tefimov85@gmail.com>
2026-04-22 21:55:18 +0300

    add postgresql

--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./job.db"
+DATABASE_URL = "postgresql+psycopg2://postgres:1234@localhost:5432/job_db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- app/db/models.py
+++ app/db/models.py
@@ -20,20 +20,20 @@ class Job(Base):
     title: Mapped[str] = mapped_column(String(128), nullable=False)
     company: Mapped[str] = mapped_column(String(128), nullable=False, index=True)
     status: Mapped[JobStatus] = mapped_column(
-        Enum(JobStatus, values_callable=lambda x: [e.value for e in x]), 
-        nullable=False, 
-        index=True
-    )
+    String(32),
+    nullable=False,
+    index=True
+)
     created_at: Mapped[datetime] = mapped_column(server_default=func.current_timestamp())
     updated_at: Mapped[datetime] = mapped_column(onupdate=func.current_timestamp(), server_default=func.current_timestamp(), nullable=True)
 
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
 
-def __repr__(self):
-    return f"<Job(id={self.id}, title={self.title!r}, company={self.company!r}, status={self.status.value})>"
+    def __repr__(self):
+        return f"<Job(id={self.id}, title={self.title!r}, company={self.company!r}, status={self.status.value})>"
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(128), nullable=False, unique=True)
